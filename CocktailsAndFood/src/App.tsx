ac9551a1f419e31f626157a9057513320007b2f1
Bejnmain <Bejnmain@5800X>
2024-03-20 15:40:09 +0100

    test test

--- CocktailsAndFood/src/App.tsx
+++ CocktailsAndFood/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <h1 className="text-5xl font-bold text-center">
           Bowls & Cocktails <span className="text-xs font-thin">(Claes)</span>
         </h1>
+        <p>Benjamin test</p>
       </div>
     </>
   );
