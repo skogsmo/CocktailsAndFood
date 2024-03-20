c8713e027db853ed64e434eac8840a0fc4aa0114
nea-rei <neaca@hotmail.com>
2024-03-20 16:00:07 +0100

    hello

--- CocktailsAndFood/src/App.tsx
+++ CocktailsAndFood/src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
       <div className="flex h-screen justify-center items-center">
-        <h3>Hello</h3>
         <h1 className="text-5xl font-bold text-center">
           Bowls & Cocktails <span className="text-xs font-thin">(Claes)</span>
         </h1>
