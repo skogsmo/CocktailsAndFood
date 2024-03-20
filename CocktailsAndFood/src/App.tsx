66d735c39c1d52217c77fd0b1f72e666c8cd3fa0
Niclas Ohlsson <niclas82@gmail.com>
2024-03-20 15:10:01 +0100

    Added Niclas

--- CocktailsAndFood/src/App.tsx
+++ CocktailsAndFood/src/App.tsx
@@ -8,13 +8,15 @@ function App() {
           Bowls & Cocktails <span className="text-xs font-thin">(Claes)</span>
         </h1>
         <button className="py-4 px-6 bg-gray-50 text-gray-600 rounded-full shadow hover:bg-gray-100 hover:shadow-md">
-          <span>
-            En knapp som kan leda någonstans i framtiden
-          </span>
-          <span className="text-xs font-thin ml-2">
-            (Benjamin)
-          </span>
+          <span>En knapp som kan leda någonstans i framtiden</span>
+          <span className="text-xs font-thin ml-2">(Benjamin)</span>
         </button>
+        <div className="flex flex-col items-center">
+          <img src="images/profile.png" />
+          <p className="text-indigo-800 hover:text-indigo-500 font-bold select-none">
+            Niclas
+          </p>
+        </div>
       </div>
     </>
   );
