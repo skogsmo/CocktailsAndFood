c24e67ddbbcf0c946618d9ccced455ab36a2321e
Claes Wikman <claes.wikman@iths.se>
2024-03-20 15:40:08 +0100

    test

--- CocktailsAndFood/src/App.tsx
+++ CocktailsAndFood/src/App.tsx
@@ -4,6 +4,7 @@ import { RecipeComp1 } from "./components/Recipe";
 function App() {
   return (
     <>
+      <p>test</p>
       <div className="flex flex-col gap-4 h-screen justify-center items-center">
         <h2 className="text-2xl text-center">
           Välkommen till <span className="text-xs font-thin">(Linnea)</span>
@@ -12,19 +13,17 @@ function App() {
           Bowls & Cocktails <span className="text-xs font-thin">(Claes)</span>
         </h1>
         <button className="py-4 px-6 bg-gray-50 text-gray-600 rounded-full shadow hover:bg-gray-100 hover:shadow-md">
-          <span>
-            En knapp som kan leda någonstans i framtiden
-          </span>
-          <span className="text-xs font-thin ml-2">
-            (Benjamin)
-          </span>
+          <span>En knapp som kan leda någonstans i framtiden</span>
+          <span className="text-xs font-thin ml-2">(Benjamin)</span>
         </button>
         <h3 className="text-xl text-center">
           Smaklig måltid! <span className="text-xs font-thin">(Lotta)</span>
-        </h3>      
-        </div>
-        <RecipeComp1></RecipeComp1>
-        <h3 className="text-x1 text-center">Alexander, testa att connecta mot API</h3>
+        </h3>
+      </div>
+      <RecipeComp1></RecipeComp1>
+      <h3 className="text-x1 text-center">
+        Alexander, testa att connecta mot API
+      </h3>
     </>
   );
 }
