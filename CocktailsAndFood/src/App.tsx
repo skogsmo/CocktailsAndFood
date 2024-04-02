65489d455a2448326f23dc24202e012ee063f412
Lotta Wikman <lotta.m.wikman@gmail.com>
2024-04-02 15:54:01 +0200

    Latestupdate

--- CocktailsAndFood/src/App.tsx
+++ CocktailsAndFood/src/App.tsx
@@ -3,7 +3,6 @@ import {Route, Routes} from "react-router-dom"
 import Start from "./pages/Start"
 import Menu from "./pages/Menu"
 import Testapi from "./pages/TutorialApiFetching"
-import { Link } from "react-router-dom";
 
 function App() {
   return (
