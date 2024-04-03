d15c8420b094df98f512ceca6608195370e30936
Systemad <dannenuke@gmail.com>
2024-04-03 18:02:35 +0200

    Update ButtonToProductPage.tsx

--- CocktailsAndFood/src/components/ButtonToProductPage.tsx
+++ CocktailsAndFood/src/components/ButtonToProductPage.tsx
@@ -2,16 +2,14 @@ import { Link } from "react-router-dom";
 
 export const ButtonToProductPage = ({
   destination,
-  children,
 }: {
   destination: string;
-  children: React.ReactNode;
 }) => {
   return (
     <>
       <Link to={`${destination}`}>
         <button className="hover:scale-105 p-2 rounded-[50px] text-[#262626] text-[16px] font-[600] text-center bg-[#FCD20A] hover:opacity-90">
-          {children}
+          View
         </button>
       </Link>
     </>
