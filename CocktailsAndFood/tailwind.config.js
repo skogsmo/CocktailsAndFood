5a91537146cb593301f7a811ee513386e3768a38
Bejnmain <Bejnmain@5800X>
2024-04-15 23:45:48 +0200

    css responsive headers etc

--- CocktailsAndFood/tailwind.config.js
+++ CocktailsAndFood/tailwind.config.js
@@ -35,6 +35,9 @@ export default {
         "custom-big":
           "0px 0px 30px 5px rgba(0,0,0,0.025), 0px 3px 5px 0px rgba(0,0,0,0.05)",
       },
+      screens: {
+        "xs": "480px",
+      },
     },
   },
   plugins: [],
