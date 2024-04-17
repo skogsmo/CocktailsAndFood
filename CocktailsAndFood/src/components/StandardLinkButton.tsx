6e3ea2b6b8b3b4636d9eea3621933e30811c2018
Bejnmain <Bejnmain@5800X>
2024-04-17 12:54:28 +0200

    StandardLinkButton kan nu acceptera "onClick" istället för "to" om det behövs (vilket det gör i DrinkSelection)

--- CocktailsAndFood/src/components/StandardLinkButton.tsx
+++ CocktailsAndFood/src/components/StandardLinkButton.tsx
@@ -1,27 +1,43 @@
 import { Link } from "react-router-dom";
 
 export default function StandardLinkButton({
-  children,
-  to,
-  yellow = false,
-  width = "w-[290px]",
+    children,
+    to,
+    onClick,
+    yellow = false,
+    width = "w-[290px]",
 }: {
-  children: string;
-  to: string;
-  yellow?: boolean;
-  width?: string;
+    children: string;
+    to?: string;
+    onClick?: () => void;
+    yellow?: boolean;
+    width?: string;
 }) {
-  const themeClasses = yellow
-    ? "border-yellow-400 hover:border-yellow-300 bg-yellow-400 hover:bg-yellow-300"
-    : "border-neutral-300 hover:bg-neutral-100";
+    if (to === undefined && onClick === undefined) {
+        console.warn("StandardLinkButton: Neither 'to' nor 'onClick' property is assigned. The button will be disabled.");
+    }
 
-    const commonClasses = "text-neutral-900 flex justify-center border-2 rounded-full p-3 text-sm font-semibold text-center";
+    const themeClasses = yellow
+        ? "border-yellow-400 hover:border-yellow-300 bg-yellow-400 hover:bg-yellow-300"
+        : "border-neutral-300 hover:bg-neutral-100";
 
-  const classes = [themeClasses, commonClasses, width].join(" ");
+    const commonClasses =
+        "text-neutral-900 flex justify-center border-2 rounded-full p-3 text-sm font-semibold text-center";
 
-  return (
-    <Link to={to} className={classes}>
-      {children}
-    </Link>
-  );
+    const classes = [themeClasses, commonClasses, width].join(" ");
+
+    return (
+        <>
+            {to && (
+                <Link to={to} className={classes}>
+                    {children}
+                </Link>
+            )}
+            {onClick && (
+                <button type="button" className={classes} onClick={onClick}>
+                    {children}
+                </button>
+            )}
+        </>
+    );
 }
--- CocktailsAndFood/src/pages/DrinkSelection.tsx
+++ CocktailsAndFood/src/pages/DrinkSelection.tsx
@@ -1,102 +1,101 @@
 import { useEffect, useState } from "react";
-import { CocktailCard } from "../components/CocktailCard";
-import { Cocktail, Order } from "../orderTypes";
-import { useNavigate } from "react-router-dom";
-import { CartModifiers } from "../App";
 import StandardLinkButton from "../components/StandardLinkButton";
-import { Cocktail, DrinkDetailsResponse, mapDrinkDetailsWithCocktail } from "../orderTypes";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import {
+    Cocktail,
+    DrinkDetailsResponse,
+    mapDrinkDetailsWithCocktail,
+} from "../orderTypes";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useOrderContext } from "../context/Context";
 
 export const DrinkSelection = () => {
+    const { currentOrder, updateOrder, isOrdersEmpty } = useOrderContext();
 
-  const {currentOrder, updateOrder, isOrdersEmpty} = useOrderContext();
+    if (isOrdersEmpty) return <Navigate to="/menu" />;
 
-  if (isOrdersEmpty) return <Navigate to="/menu" />;
-  
-  const navigate = useNavigate();
-  const [formattedDrink, setFormattedDrink] = useState<Cocktail | undefined>(
-    undefined
-  );
-  const [cocktailId] = useState(() => {
-    switch (currentOrder.Protein?.Id) {
-      case 1:
-        return "11410";
-      case 2:
-        return "12198";
-      case 3:
-        return "11422";
-      case 4:
-        return "13731";
-      case 5:
-        return "12690";
-      default:
-        break;
-    }
-  });
+    const navigate = useNavigate();
+    const [formattedDrink, setFormattedDrink] = useState<Cocktail | undefined>(
+        undefined
+    );
+    const [cocktailId] = useState(() => {
+        switch (currentOrder.Protein?.Id) {
+            case 1:
+                return "11410";
+            case 2:
+                return "12198";
+            case 3:
+                return "11422";
+            case 4:
+                return "13731";
+            case 5:
+                return "12690";
+            default:
+                break;
+        }
+    });
 
-  useEffect(() => {
-    const getCocktails = async () => {
-      const response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`
-      );
-      const data: DrinkDetailsResponse = await response.json();
-      setFormattedDrink(mapDrinkDetailsWithCocktail(data));
+    useEffect(() => {
+        const getCocktails = async () => {
+            const response = await fetch(
+                `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`
+            );
+            const data: DrinkDetailsResponse = await response.json();
+            setFormattedDrink(mapDrinkDetailsWithCocktail(data));
+        };
+        getCocktails();
+    }, [cocktailId]);
+
+    const handleClick = () => {
+        const updatedOrder = {
+            ...currentOrder,
+            Cocktail: formattedDrink,
+        };
+
+        updateOrder(updatedOrder);
+        navigate("/checkout");
     };
-    getCocktails();
-  }, [cocktailId]);
 
-  const handleClick = () => {
-    const updatedOrder = {
-      ...currentOrder,
-      Cocktail: formattedDrink,
-    };
+    return (
+        <>
+            <div className="main-wrapper">
+                <div className="px-8 md:px-0 text-center flex flex-col gap-5 mb-[50px]">
+                    <h2>Din cocktailrekommendation</h2>
+                    <p className="text-lg">
+                        Låt dig inspireras av vårt förslag eller valj att skapa
+                        din egen unika smakresa genom att byta ut
+                        rekommendationen mot en annan cocktail från vår meny.
+                    </p>
+                </div>
 
-    updateOrder(updatedOrder);
-    navigate("/checkout");
-  };
-  // <CocktailCard cocktail={formattedDrink} />
-  return (
-    <>
-      <div className="main-wrapper">
-        <div className="px-8 md:px-0 text-center flex flex-col gap-5 mb-[50px]">
-          <h2>Din cocktailrekommendation</h2>
-          <p className="text-lg">
-            Låt dig inspireras av vårt förslag eller valj att skapa din egen
-            unika smakresa genom att byta ut rekommendationen mot en annan
-            cocktail från vår meny.
-          </p>
-        </div>
+                {formattedDrink && (
+                    <div className="w-full md:rounded-[25px] overflow-hidden bg-white shadow-custom-big">
+                        <div className="w-full flex flex-col-reverse gap-4 items-center md:flex-row justify-between p-8">
+                            <img
+                                className="h-[350px] w-full object-fit md:rounded-[25px]"
+                                src={formattedDrink.ImgUrl}
+                            />
+                            <div>
+                                <h2>{formattedDrink.CocktailName}</h2>
+                                <p className="font-semibold my-[15px]">
+                                    {formattedDrink.Price.toFixed(2)} kr
+                                </p>
+                                <p>{formattedDrink.Description}</p>
+                            </div>
+                        </div>
 
-        {formattedDrink && (
-          <div className="w-full md:rounded-[25px] overflow-hidden bg-white shadow-custom-big">
-            <div className="w-full flex flex-col-reverse gap-4 items-center md:flex-row justify-between p-8">
-              <img
-                className="h-[350px] w-full object-fit md:rounded-[25px]"
-                src={formattedDrink.ImgUrl}
-              />
-              <div>
-                <h2>{formattedDrink.CocktailName}</h2>
-                <p className="font-semibold my-[15px]">
-                  {formattedDrink.Price.toFixed(2)} kr
-                </p>
-                <p>{formattedDrink.Description}</p>
-              </div>
+                        <hr className="border-neutral-300 border-t" />
+
+                        <div className="w-full flex flex-col-reverse gap-4 items-center md:flex-row justify-between p-8">
+                            <StandardLinkButton to={"/drinklist"}>
+                                Gå till drinkmenyn
+                            </StandardLinkButton>
+                            <StandardLinkButton onClick={handleClick} yellow>
+                                Acceptera förslag
+                            </StandardLinkButton>
+                        </div>
+                    </div>
+                )}
             </div>
-
-            <hr className="border-neutral-300 border-t" />
-
-            <div className="w-full flex flex-col-reverse gap-4 items-center md:flex-row justify-between p-8">
-              <StandardLinkButton to={"/drinklist"}>
-                Gå till drinkmenyn
-              </StandardLinkButton>
-              <button onClick={handleClick} className="border-yellow-400 hover:border-yellow-300 bg-yellow-400 hover:bg-yellow-300 border-2 rounded-full p-3 text-sm font-semibold text-center w-[290px]">
-                Acceptera förslag
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 };
